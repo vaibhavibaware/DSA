edc964db01f77aa438935de9dc4fe91cdc5a1322
Vaibhavi Baware <vaibhavibaware09@gmail.com>
2025-06-08 00:27:16 +0530

    Update MinimumAddtoMakeParenthesesValid.java

--- MinimumAddtoMakeParenthesesValid.java
+++ MinimumAddtoMakeParenthesesValid.java
@@ -3,28 +3,18 @@ Question:
  * Given a string S of '(' and ')' parentheses, we add
  * the minimum number of parentheses ( '(' or ')', and in any positions ) so that the resulting
  * parentheses string is valid.
- *
  * <p>Formally, a parentheses string is valid if and only if:
- *
  * <p>It is the empty string, or It can be written as AB (A concatenated with B), where A and B are
  * valid strings, or It can be written as (A), where A is a valid string. Given a parentheses
  * string, return the minimum number of parentheses we must add to make the resulting string valid.
- *
- Explanation:
+ *Explanation:
  * <p>Example 1:
- *
  * <p>Input: "())" Output: 1 Example 2:
- *
  * <p>Input: "(((" Output: 3 Example 3:
- *
  * <p>Input: "()" Output: 0 Example 4:
- *
  * <p>Input: "()))((" Output: 4
- *
  * <p>Note:
- *
  * <p>S.length <= 1000 S only consists of '(' and ')' characters.
- *
  * <p>Solution O(N) Keep track of count of open parentheses, when ever a closed parentheses appear
  * if the count of open parentheses is greater than 0 then decrement this value (identifying that
  * there is a matching parentheses already), if the count is 0 then there is a miss match with
