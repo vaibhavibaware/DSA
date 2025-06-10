ea8657b0dc2d8af08ee32e536cd95feb75ea2970
Vaibhavi Baware <vaibhavibaware09@gmail.com>
2025-06-11 01:18:26 +0530

    Update StudentAttendanceRecordI.java

--- StudentAttendanceRecordI.java
+++ StudentAttendanceRecordI.java
@@ -1,6 +1,6 @@
 //Question:
 /**
- * You are given a string representing an attendance
+ * You are given a string representing an attendance 
  * record for a student. The record only contains the following three characters: 'A' : Absent. 'L'
  * : Late. 'P' : Present. A student could be rewarded if his attendance record doesn't contain more
  * than one 'A' (absent) or more than two continuous 'L' (late).
