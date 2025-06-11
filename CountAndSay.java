e72ab823221a254060c67d8252bbf40ea121c3a5
Vaibhavi Baware <vaibhavibaware09@gmail.com>
2025-06-12 01:41:32 +0530

    Update CountAndSay.java

--- CountAndSay.java
+++ CountAndSay.java
@@ -1,6 +1,6 @@
 Question:
 /**
- * <p>The count-and-say sequence is the sequence of integers with the first five terms as following:
+ * <p>The count-and-say sequence is  the sequence of integers with the first five terms as following:
  *
  * <p>1. 1 2. 11 3. 21 4. 1211 5. 111221 1 is read off as "one 1" or 11. 11 is read off as "two 1s"
  * or 21. 21 is read off as "one 2, then one 1" or 1211. Given an integer n, generate the nth term
