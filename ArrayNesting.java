af216e084739691728688cdad66fc1ed92f70b33
Vaibhavi Baware <vaibhavibaware09@gmail.com>
2025-06-15 00:02:52 +0530

    Update ArrayNesting.java

--- ArrayNesting.java
+++ ArrayNesting.java
@@ -14,11 +14,8 @@ import java.util.*;
  *
  * <p>Input: A = [5,4,0,3,1,6,2] Output: 4 Explanation: A[0] = 5, A[1] = 4, A[2] = 0, A[3] = 3, A[4]
  * = 1, A[5] = 6, A[6] = 2.
- *
  * <p>One of the longest S[K]: S[0] = {A[0], A[5], A[6], A[2]} = {5, 6, 2, 0}
- *
  * <p>Note:
- *
  * <p>N is an integer within the range [1, 20,000]. The elements of A are all distinct. Each element
  * of A is an integer within the range [0, N-1].
  */
